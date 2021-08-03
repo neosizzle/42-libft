d6c2a4cb1d7cc46d4b6161722e6e58dd592782e1
neosizzle <junhanngd1115@gmail.com>
2021-08-03 12:16:37 +0100

    asdfd

--- ft_memchr.c
+++ ft_memchr.c
@@ -2,15 +2,14 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
+	size_t i;
 
 	i = 0;
 	while (i < n)
 	{
-		if (*(unsigned char *)s == (unsigned char)c)
-			return ((void *)s);
+		if (((unsigned char*)s)[i] == (unsigned char)c)
+			return ((void*)(s + i));
 		i++;
-		s++;
 	}
 	return (0);
 }
