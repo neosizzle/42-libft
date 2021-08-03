91923a973d5f8e988b8ae6e3a798f48a5ccaee8f
neosizzle <junhanngd1115@gmail.com>
2021-08-03 12:21:30 +0100

    memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -4,8 +4,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	if (!n)
 		return (0);
-	while ((*(unsigned char *)s1)
-		&& (*(unsigned char *)s1 == *(unsigned char *)s2) && --n)
+	while ((*(unsigned char *)s1 == *(unsigned char *)s2) && --n)
 	{
 		s1++;
 		s2++;
