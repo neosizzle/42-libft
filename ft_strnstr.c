62ca4de0528c5eb9ac8a66c5cf1ac05fdbb437b1
neosizzle <junhanngd1115@gmail.com>
2021-08-03 13:43:38 +0100

    norm

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -16,7 +16,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	{
 		c = 0;
 		while (hay[i + c] && little[c]
-				&& hay[i + c] == little[c] && i + c < len)
+			&& hay[i + c] == little[c] && i + c < len)
 			c++;
 		if (c == res_len)
 			return (hay + i);
